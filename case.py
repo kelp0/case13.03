55d6194d6c890c6ca05b86f3f125eb3574c89b99
kelp0 <93107543+kelp0@users.noreply.github.com>
2022-04-10 20:36:11 +0700

    Update case.py

--- case.py
+++ case.py
@@ -1,6 +1,7 @@
+#Работу выполнили: Лысенко Матвей 70%, Скороходов Михаил 40% гр.21704.1
 import turtle
 from math import sqrt
-def get_color_choice():
+def get_color_choice(): #функция выбора цвета
     print('Допустимые цвета заливки: ')
     print('Фиолетовый')
     print('Красный')
@@ -25,7 +26,7 @@ def get_color_choice():
     else:
         a = input('Не является верным значением. Пожалуйста, повторите попытку: ')
         return get_color_choice()
-def get_num_hexagons():
+def get_num_hexagons(): #Функция выбора количества шестиугольников в ряду
     b = input('Пожалуйста, введите количество шестиугольников, располагаемых в ряд: ')
     while ValueError:
         try:
@@ -42,7 +43,7 @@ def get_num_hexagons():
             except ValueError:
                 b = input('Оно должно быть от 4 до 20. Пожалуйста, повторите попытку: ')
     return int(b)
-def draw_hexagon(x,y,side_len,color):
+def draw_hexagon(x,y,side_len,color): #Функция рисования шестиугольника
     turtle.speed(1000)
     turtle.setposition(x,y)
     turtle.fillcolor(color)
@@ -77,8 +78,8 @@ s = 3*sqrt(3)*side_len*side_len/2
 move = sqrt(s*2/sqrt(3))
 move2 = side_len*side_len - (side_len*side_len/4)
 ccc = ''
-while j != num+1:
-    while i != num+1:
+while j != num+1: #Цикл, смещающий turtle по оси ординат
+    while i != num+1: #Цикл, описывающий линию шестиугольников
         if i % 2 == 1:
             color = color1
         elif i % 2 == 0:
@@ -97,10 +98,7 @@ while j != num+1:
         x = x + sqrt(move2)
         y = y - side_len/2
     if j % 2 == 0:
-        ccc = color1
+        ccc = color1 #Смена цвета для получения необходимого орнамента
         color1 = color2
         color2 = ccc
     j += 1
-
-    
-    
